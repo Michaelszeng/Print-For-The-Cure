7cae0c766cd60c0334a632aa0654457b27c62c1c
Michaelszeng <michael.zeng.22@bishops.com>
2020-06-08 20:11:57 -0700

    added hand-made/laser shields, finalized packing script, updated sponsors list, fixed meet the team spacing

--- PrintForTheCure/main/shippingSheetGenerator.py
+++ PrintForTheCure/main/shippingSheetGenerator.py
@@ -2,22 +2,52 @@ import xlwt
 from xlwt import Workbook
 from ShippingInfo_6_4_20 import *   #change depending on shipping day
 
-workbook = Workbook()
-sheet = workbook.add_sheet('6.4.20')
+workbookBox = Workbook()
+workbookBag = Workbook()
+sheetBox = workbookBox.add_sheet('6.7.20')
+sheetBag = workbookBag.add_sheet('6.7.20')
 style = xlwt.easyxf('font: bold 1')
 
 # row, column
-sheet.write(0, 0, 'Full Name', style)
-sheet.write(0, 1, 'Address1', style)
-sheet.write(0, 2, 'City', style)
-sheet.write(0, 3, 'State', style)
-sheet.write(0, 4, 'Zipcode', style)
+sheetBox.write(0, 0, 'Full Name', style)
+sheetBox.write(0, 1, 'Address1', style)
+sheetBox.write(0, 2, 'City', style)
+sheetBox.write(0, 3, 'State', style)
+sheetBox.write(0, 4, 'Zipcode', style)
+sheetBox.write(0, 5, 'Type PPE', style)
+sheetBox.write(0, 6, 'Amount', style)
+sheetBox.write(0, 7, 'Email', style)
+
+sheetBag.write(0, 0, 'Full Name', style)
+sheetBag.write(0, 1, 'Address1', style)
+sheetBag.write(0, 2, 'City', style)
+sheetBag.write(0, 3, 'State', style)
+sheetBag.write(0, 4, 'Zipcode', style)
+sheetBag.write(0, 5, 'Type PPE', style)
+sheetBag.write(0, 6, 'Amount', style)
+sheetBag.write(0, 7, 'Email', style)
 style = xlwt.easyxf('font: bold off')
 
 currentRow = 1
 for i in range(len(data)-22):
+    emailSearcher = data[i:i+19]
     nameSearcher = data[i:i+18]
     addressSearcher = data[i:i+21]
+    typePPESearcher = data[i:i+23]
+    numSearcher = data[i:i+25]
+
+    email = ""
+    if emailSearcher == "Requester's Email: ":
+        iCounterStartValue = i
+        iCounter = i
+        while True:
+            #name ends when the next 19 chars are "Requester's Email: "
+            if "Requester's Phone Number:" in data[19+iCounter:19+iCounter+25] or iCounter-iCounterStartValue > 80:   #max email length = 80
+                break
+            email += data[19+iCounter:20+iCounter]
+            iCounter += 1
+        email2 = email    #have to make a new var bc for some reason the value of name always gets reudced to an empty string somehow
+
 
     name = ""
     if nameSearcher == "Requester's Name: ":
@@ -29,8 +59,8 @@ for i in range(len(data)-22):
                 break
             name += data[18+iCounter:19+iCounter]
             iCounter += 1
+        name2 = name    #have to make a new var bc for some reason the value of name always gets reudced to an empty string somehow
 
-        sheet.write(currentRow, 0, name, style)
 
     fullAddress = ""
     if addressSearcher == "Requester's Address: ":
@@ -45,27 +75,127 @@ for i in range(len(data)-22):
 
 
         addressList = fullAddress.split(" ")
-        for i in range(len(addressList)-1):
-            if len(addressList[i]) == 0:
-                addressList.pop(i)
+        # print(addressList)
+        safetyCounter = 0
+        while True:
+            found = False
+            for i in range(len(addressList)-1):
+                #removing periods from address item
+                addressItemNoPeriod = ""
+                for char in addressList[i]:
+                    if char != ".":
+                        addressItemNoPeriod += char
+                addressList[i] = addressItemNoPeriod
+
+                #deleting address items that are just blank spaces
+                if len(addressList[i]) == 0:
+                    addressList.pop(i)
+                    found = True
+                    break
+            if found == False or safetyCounter > 5:
+                break
+            safetyCounter += 1
         print(addressList)
         zip = addressList[len(addressList)-2]
         state = addressList[len(addressList)-3]
-        if "," in addressList[len(addressList)-5]:
+        notCityThings  = ["str", "st", "street", "ave", "avenue", "blvd", "boulevard", "rd", "road", "ln", "lane", "dr", "drive", "ct", "court", "way", "pl", "place", "pkwy", "parkway", "cir", "circle", "ctr", "cntr", "center", "plaza"]
+        if len(addressList[len(addressList)-5]) <= 2 and addressList[len(addressList)-5].lower() != "of":   #if it is likely an apt/suite number
             city = addressList[len(addressList)-4]
             separator = " "
             address = separator.join(addressList[0:len(addressList)-4])
         else:
-            city = addressList[len(addressList)-5] + " " + addressList[len(addressList)-4]
-            separator = " "
-            address = separator.join(addressList[0:len(addressList)-5])
+            try:
+                int(addressList[len(addressList)-5])
+                city = addressList[len(addressList)-4]
+                separator = " "
+                address = separator.join(addressList[0:len(addressList)-4])
+            except:
+                partOfCityName = True
+                for thing in notCityThings:     #a road, street, etc.
+                    if addressList[len(addressList)-5].lower() == thing:
+                        city = addressList[len(addressList)-4]
+                        separator = " "
+                        address = separator.join(addressList[0:len(addressList)-4])
+                        partOfCityName = False
 
-        sheet.write(currentRow, 4, zip, style)
-        sheet.write(currentRow, 3, state, style)
-        sheet.write(currentRow, 2, city.replace(',', ''), style)
-        sheet.write(currentRow, 1, address.replace(',', ''), style)
+                if partOfCityName == True:
+                    city = addressList[len(addressList)-5] + " " + addressList[len(addressList)-4]
+                    separator = " "
+                    address = separator.join(addressList[0:len(addressList)-5])
+
+    typePPE=""
+    if typePPESearcher == "Type of PPE Requested: ":
+        iCounterStartValue = i
+        iCounter = i
+        while True:
+            if "Amount of PPE Requested: " in data[23+iCounter:23+iCounter+25] or iCounter-iCounterStartValue > 120:   #max typePPE length = 120
+                break
+            typePPE += data[iCounter+23:iCounter+24]
+            iCounter += 1
+        type2 = typePPE     #have to make a new var bc for some reason the value of typePPE always gets reudced to an empty string somehow
+        # print(typePPE)
+
+
+    num=""
+    if numSearcher == "Amount of PPE Requested: ":
+        iCounterStartValue = i
+        iCounter = i
+        while True:
+            if " date for the requested PPE: " in data[25+18+iCounter:25+18+iCounter+29] or iCounter-iCounterStartValue > 4:   #max num length = 4
+                break
+            num += data[iCounter+25:iCounter+26]
+            iCounter += 1
+
+
+        numbers = '1234567890'
+        numPunct = num
+        num = ""
+        for char in numPunct:
+           if char in numbers:
+               num = num + char
+
+        num = int(num)
+        # print("typePPE: " + type2)
+        if "3D Printed Face Shields" in type2:
+            packageType = "Box"
+        elif "Personal Touchless Door Opener" in type2:
+            if num > 3:
+                packageType = "Box"
+            else:
+                packageType = "Bag"
+        elif "Face Mask Comfort Strap" in type2:
+            if num > 125:
+                packageType = "Box"
+            else:
+                packageType = "Bag"
+        elif "Touch-less Door Handle" in type2:
+            if num > 1:
+                packageType = "Box"
+            else:
+                packageType = "Bag"
+        # print(packageType)
+
+        if packageType == "Box":
+            sheetBox.write(currentRow, 7, email2, style)
+            sheetBox.write(currentRow, 6, num, style)
+            sheetBox.write(currentRow, 5, type2, style)
+            sheetBox.write(currentRow, 4, zip, style)
+            sheetBox.write(currentRow, 3, state, style)
+            sheetBox.write(currentRow, 2, city.replace(',', ''), style)
+            sheetBox.write(currentRow, 1, address.replace(',', ''), style)
+            sheetBox.write(currentRow, 0, name2, style)
+        elif packageType == "Bag":
+            sheetBag.write(currentRow, 7, email2, style)
+            sheetBag.write(currentRow, 6, num, style)
+            sheetBag.write(currentRow, 5, type2, style)
+            sheetBag.write(currentRow, 4, zip, style)
+            sheetBag.write(currentRow, 3, state, style)
+            sheetBag.write(currentRow, 2, city.replace(',', ''), style)
+            sheetBag.write(currentRow, 1, address.replace(',', ''), style)
+            sheetBag.write(currentRow, 0, name2, style)
         currentRow += 1
 
 
 
-workbook.save("Shipping.xls")
+workbookBox.save("ShippingBoxes.xls")
+workbookBag.save("ShippingEnvelopes.xls")
